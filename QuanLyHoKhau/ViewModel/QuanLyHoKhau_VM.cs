4d892578572f097aed929961d327b6299d9fa2b9
hcthcm290 <dragonnica123@gmail.com>
2020-12-27 15:34:35 +0700

    Re-init the project

---
+++ QuanLyHoKhau/ViewModel/QuanLyHoKhau_VM.cs
@@ -0,0 +1,23 @@
+﻿using QuanLyHoKhau.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace QuanLyHoKhau.ViewModel
+{
+    class QuanLyHoKhau_VM : BaseViewModel
+    {
+        public ICommand BtnAddSoHoKhau_Command { get; set; }
+
+        public QuanLyHoKhau_VM()
+        {
+            BtnAddSoHoKhau_Command = new RelayCommand((p) => {
+                NhapHoKhauWindow nhapHoKhauWindow = new NhapHoKhauWindow();
+                nhapHoKhauWindow.ShowDialog();
+            });
+        }
+    }
+}
---
+++ QuanLyHoKhau/ViewModel/QuanLyNhanKhau_VM.cs
@@ -0,0 +1,23 @@
+﻿using QuanLyHoKhau.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace QuanLyHoKhau.ViewModel
+{
+    class QuanLyNhanKhau_VM : BaseViewModel
+    {
+        public ICommand BtnAddNhanKhau_Command { get; set; }
+
+        public QuanLyNhanKhau_VM()
+        {
+            BtnAddNhanKhau_Command = new RelayCommand((p) => {
+                NhapNhanKhauWindow nhapNhanKhauWindow = new NhapNhanKhauWindow();
+                nhapNhanKhauWindow.ShowDialog();
+            });
+        }
+    }
+}
