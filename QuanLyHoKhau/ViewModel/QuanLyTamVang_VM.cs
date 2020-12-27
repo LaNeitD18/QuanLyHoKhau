8d0d5db3ee27df3a270c8ec920bfeda38f5698c0
hcthcm290 <dragonnica123@gmail.com>
2020-12-28 00:02:41 +0700

    Add ability to filter ListPhieuTamVang in QuanLyTamVang screen

--- QuanLyHoKhau/ViewModel/QuanLyTamVang_VM.cs
+++ QuanLyHoKhau/ViewModel/QuanLyTamVang_VM.cs
@@ -1,10 +1,12 @@
 ﻿using QuanLyHoKhau.View;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using QuanLyHoKhau.Model;
 
 namespace QuanLyHoKhau.ViewModel
 {
@@ -18,6 +20,47 @@ namespace QuanLyHoKhau.ViewModel
                 NhapGiayTamVangWindow nhapGiayTamVangWindow = new NhapGiayTamVangWindow();
                 nhapGiayTamVangWindow.ShowDialog();
             });
+        }
+
+        ObservableCollection<PHIEUKHAIBAOTAMVANG> _listPhieuTamVang;
+        public ObservableCollection<PHIEUKHAIBAOTAMVANG> ListPhieuTamVang
+        {
+            get
+            {
+                if(_listPhieuTamVang == null)
+                {
+                    _listPhieuTamVang = new ObservableCollection<PHIEUKHAIBAOTAMVANG>(DataProvider.Ins.DB.PHIEUKHAIBAOTAMVANGs.ToList());
+                }
+                return _listPhieuTamVang;
+            }
+            set
+            {
+                _listPhieuTamVang = value;
+            }
+        }
+
+        string _searchText;
+        public string SearchText
+        {
+            get { return _searchText; }
+            set 
+            { 
+                _searchText = value; 
+            }
+        }
+
+        public void FilterListPhieuTamVang()
+        {
+            if(_searchText == "" || _searchText == null)
+            {
+                _listPhieuTamVang = new ObservableCollection<PHIEUKHAIBAOTAMVANG>(DataProvider.Ins.DB.PHIEUKHAIBAOTAMVANGs.ToList());
+            }
+            else
+            {
+                List<PHIEUKHAIBAOTAMVANG> listPhieuTamVangFiltered = DataProvider.Ins.DB.PHIEUKHAIBAOTAMVANGs.Where(x => x.MaPhieuKhaiBao.Contains(_searchText)).ToList();
+                _listPhieuTamVang = new ObservableCollection<PHIEUKHAIBAOTAMVANG>(listPhieuTamVangFiltered);
+            }
+            OnPropertyChanged("ListPhieuTamVang");
         }
     }
 }
