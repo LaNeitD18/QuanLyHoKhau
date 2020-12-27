dc0d262790ddcf5693f2e42b829e132c7088f22d
LaNeitD18 <phanhuytien14@gmail.com>
2020-12-27 16:31:53 +0700

    Finish login feature

---
+++ QuanLyHoKhau/CustomControls/TextboxWithPlaceHolder.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuanLyHoKhau.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for TextboxWithPlaceHolder.xaml
+    /// </summary>
+    public partial class TextboxWithPlaceHolder : UserControl
+    {
+        public TextboxWithPlaceHolder()
+        {
+            InitializeComponent();
+        }
+
+        public string PlaceHolder
+        {
+            get { return (string)GetValue(PlaceHolderProperty); }
+            set { SetValue(PlaceHolderProperty, value); }
+        }
+
+        public static readonly DependencyProperty PlaceHolderProperty =
+            DependencyProperty.Register("PlaceHolder", typeof(string), typeof(TextboxWithPlaceHolder));
+
+
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(TextboxWithPlaceHolder));
+
+
+
+        public bool IsPassword
+        {
+            get { return (bool)GetValue(IsPasswordProperty); }
+            set { SetValue(IsPasswordProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsPasswordBox.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsPasswordProperty =
+            DependencyProperty.Register("IsPassword", typeof(bool), typeof(TextboxWithPlaceHolder));
+    }
+}
